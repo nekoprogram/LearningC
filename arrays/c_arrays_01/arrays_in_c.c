80a0bcffd1703140ede70d3c10af85d5c0bf9f04
nekoprogram <nekoprogramu@gmail.com>
2024-07-10 23:16:05 +0200

    Example arrays in C

--- arrays/c_arrays_01/arrays_in_c.c
+++ arrays/c_arrays_01/arrays_in_c.c
@@ -3,6 +3,13 @@
 
 int main()
 {
+    int array[2];
+    array[0] = 20;
+    array[1] = 15;
+    
+    printf("Primer valor de la tabla:\t%d\nSegundo valor de la tabla:\t%d\n",array[0],array[1]);
+    printf("Suma de los dos valores:\t%d",(array[0]+array[1]));
+    printf("\n");
 
     return 0;
 }
